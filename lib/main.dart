3a26271b377f5c6afdd6dc2a4dc565cdd2e3dc91
Harrem <52822616+Harrem@users.noreply.github.com>
2022-09-29 19:38:52 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Messenger',
-      theme: CustomTheme().lightTheme,
+      theme: CustomTheme().darkTheme,
       home: const CreateProfile(),
     );
   }
