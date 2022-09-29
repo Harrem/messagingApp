489bfdaf8eab18adf5405e297e49812ecca4b864
Harrem <52822616+Harrem@users.noreply.github.com>
2022-09-29 19:38:29 +0300

    fixed screen overflow when keyboard is active

--- lib/screens/create_profile.dart
+++ lib/screens/create_profile.dart
@@ -19,105 +19,118 @@ class _CreateProfileState extends State<CreateProfile> {
   TextEditingController firstNameText = TextEditingController();
   TextEditingController lastNameText = TextEditingController();
   bool? lightTheme;
+  double? height;
   @override
   Widget build(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
     lightTheme = Theme.of(context).brightness == Brightness.dark;
-    return Container(
-      decoration: lightTheme!
-          ? const BoxDecoration(
-              gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color.fromARGB(255, 40, 80, 110),
-                Color.fromARGB(255, 23, 47, 73),
-              ],
-            ))
-          : const BoxDecoration(),
-      child: Scaffold(
-        backgroundColor: !lightTheme! ? Colors.white : Colors.transparent,
-        body: Center(
-          child: Container(
-            constraints: const BoxConstraints(maxWidth: 700),
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ClipOval(
-                  child: Container(
-                    height: 40,
-                    width: 40,
-                    color: Colors.grey,
-                  ),
-                ),
-                const SizedBox(height: 100),
-                Text(
-                  "Enter Your Name",
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                Text(
-                  "Fill in to continue",
-                  style: Theme.of(context).textTheme.subtitle2,
-                ),
-                const SizedBox(height: 60),
-                TextFormField(
-                  controller: firstNameText,
-                  decoration: const InputDecoration(labelText: "FIRST NAME"),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: lastNameText,
-                  decoration: const InputDecoration(labelText: "LAST NAME"),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () async {
-                    var email = firstNameText.text;
-                    var password = lastNameText.text;
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                CreateProfileSecondPage()));
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text("PROCEED"),
-                      Icon(Icons.arrow_right_rounded),
+    return Scaffold(
+      backgroundColor: !lightTheme! ? Colors.white : Colors.transparent,
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: lightTheme!
+              ? const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color.fromARGB(255, 40, 80, 110),
+                      Color.fromARGB(255, 23, 47, 73),
                     ],
                   ),
-                ),
-                const SizedBox(height: 10),
-                Expanded(
-                  child: Container(
-                    alignment: Alignment.bottomCenter,
-                    // color: Colors.white,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                          onPressed: () {},
-                          child: const Text("Help"),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    const SignUp(),
-                              ),
-                            );
-                          },
-                          child: const Text("Sign In"),
-                        )
-                      ],
-                    ),
-                  ),
                 )
-              ],
+              : const BoxDecoration(),
+          child: Center(
+            child: SizedBox(
+              height: height,
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 40,
+                      width: 40,
+                      child: ClipOval(
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 100),
+                    Text(
+                      "Enter Your Name",
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    Text(
+                      "Fill in to continue",
+                      style: Theme.of(context).textTheme.subtitle2,
+                    ),
+                    const SizedBox(height: 60),
+                    TextFormField(
+                      controller: firstNameText,
+                      decoration:
+                          const InputDecoration(labelText: "FIRST NAME"),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: lastNameText,
+                      decoration: const InputDecoration(labelText: "LAST NAME"),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () async {
+                        var email = firstNameText.text;
+                        var password = lastNameText.text;
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    CreateProfileSecondPage()));
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("PROCEED"),
+                          Icon(Icons.arrow_right_rounded),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: Container(
+                        alignment: Alignment.bottomCenter,
+                        // color: Colors.white,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text("Help"),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        const SignUp(),
+                                  ),
+                                );
+                              },
+                              child: const Text("Sign In"),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
         ),
