8603ff12136e974abeed28eb2f22c64c507fe43e
Harrem <52822616+Harrem@users.noreply.github.com>
2022-10-02 13:01:09 +0300

    fixed layouts

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,159 +1,157 @@
-import 'package:assignment/screens/home.dart';
-import 'package:assignment/controller/authentication.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:assignment/controller/authentication.dart';
+import 'package:provider/provider.dart';
+import '../route.dart';
 
-import 'sign_up.dart';
+class SignIn extends StatelessWidget {
+  SignIn({super.key});
 
-class SignIn extends StatefulWidget {
-  const SignIn({Key? key}) : super(key: key);
-
-  @override
-  State<SignIn> createState() => _SignInState();
-}
-
-class _SignInState extends State<SignIn> {
   final GlobalKey<FormState> formkey = GlobalKey<FormState>();
-  final Auth auth = Auth();
-  TextEditingController usernameText = TextEditingController();
-  TextEditingController passwordText = TextEditingController();
-  bool? lightTheme;
+  final TextEditingController usernameText = TextEditingController();
+  final TextEditingController passwordText = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    lightTheme = Theme.of(context).brightness == Brightness.dark;
-    return Container(
-      decoration: lightTheme!
-          ? const BoxDecoration(
-              gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color.fromARGB(255, 40, 80, 110),
-                Color.fromARGB(255, 23, 47, 73),
-              ],
-            ))
-          : const BoxDecoration(),
-      child: Scaffold(
-        backgroundColor: !lightTheme! ? Colors.white : Colors.transparent,
-        body: Center(
-          child: Container(
-            constraints: const BoxConstraints(maxWidth: 700),
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ClipOval(
-                  child: Container(
-                    height: 40,
-                    width: 40,
-                    color: Colors.grey,
-                  ),
-                ),
-                const SizedBox(height: 100),
-                Text(
-                  "Welcome Back",
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                Text(
-                  "Sign in to continue",
-                  style: Theme.of(context).textTheme.subtitle2,
-                ),
-                const SizedBox(height: 60),
-                TextFormField(
-                  controller: usernameText,
-                  decoration: const InputDecoration(labelText: "USERNAME"),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: passwordText,
-                  obscureText: true,
-                  decoration: const InputDecoration(labelText: "PASSWORD"),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () async {
-                    var email = usernameText.text;
-                    var password = passwordText.text;
+    final auth = Provider.of<Auth>(context);
+    bool lightTheme = Theme.of(context).brightness == Brightness.dark;
 
-                    await auth
-                        .signInWithEmailAndPassword(
-                            email: email, password: password)
-                        .onError(
-                      (error, stackTrace) async {
-                        bool isShown = true;
-                        ScaffoldMessenger.of(context).showMaterialBanner(
-                          MaterialBanner(
-                            content: Text("$error"),
-                            actions: [
-                              //remove banner when pressed
-                              TextButton(
-                                onPressed: () async {
-                                  isShown = false;
-                                  ScaffoldMessenger.of(context)
-                                      .clearMaterialBanners();
-                                },
-                                child: const Text("Dismiss"),
-                              )
-                            ],
-                          ),
-                        );
-                        // remove banner after 3 seconds
-                        if (isShown) {
-                          await Future.delayed(const Duration(seconds: 3));
-                          // ignore: use_build_context_synchronously
-                          ScaffoldMessenger.of(context).clearMaterialBanners();
-                        }
-                        return null;
-                      },
-                    );
-                    if (auth.checkUser()) {
-                      // ignore: use_build_context_synchronously
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (BuildContext context) => Home(),
-                        ),
-                      );
-                    }
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text("PROCEED"),
-                      Icon(Icons.arrow_right_rounded),
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        backgroundColor: lightTheme ? Colors.white : Colors.transparent,
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: lightTheme
+                ? const BoxDecoration(
+                    gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color.fromARGB(255, 40, 80, 110),
+                      Color.fromARGB(255, 23, 47, 73),
                     ],
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Expanded(
-                  child: Container(
-                    alignment: Alignment.bottomCenter,
-                    // color: Colors.white,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                          onPressed: () {},
-                          child: const Text("Forgot Password?"),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    const SignUp(),
+                  ))
+                : const BoxDecoration(),
+            child: Center(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Divider(),
+                    ClipOval(
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(height: 100),
+                    Text(
+                      "Welcome Back",
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    Text(
+                      "Sign in to continue",
+                      style: Theme.of(context).textTheme.subtitle2,
+                    ),
+                    const SizedBox(height: 60),
+                    TextFormField(
+                      controller: usernameText,
+                      decoration: const InputDecoration(labelText: "USERNAME"),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: passwordText,
+                      obscureText: true,
+                      decoration: const InputDecoration(labelText: "PASSWORD"),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () async {
+                        var email = usernameText.text.trim();
+                        var password = passwordText.text;
+
+                        await auth
+                            .signInWithEmailAndPassword(
+                                email: email, password: password)
+                            .onError(
+                          (error, stackTrace) async {
+                            bool isShown = true;
+                            ScaffoldMessenger.of(context).showMaterialBanner(
+                              MaterialBanner(
+                                content: Text("$error"),
+                                actions: [
+                                  //remove banner when pressed
+                                  TextButton(
+                                    onPressed: () async {
+                                      isShown = false;
+                                      ScaffoldMessenger.of(context)
+                                          .clearMaterialBanners();
+                                    },
+                                    child: const Text("Dismiss"),
+                                  )
+                                ],
                               ),
                             );
+                            // remove banner after 3 seconds
+                            if (isShown) {
+                              await Future.delayed(const Duration(seconds: 3));
+                              // ignore: use_build_context_synchronously
+                              ScaffoldMessenger.of(context)
+                                  .clearMaterialBanners();
+                            }
                           },
-                          child: const Text("Create Account"),
-                        )
-                      ],
+                        );
+                        if (auth.checkUser()) {
+                          // ignore: use_build_context_synchronously
+                          Navigator.of(context).pushNamed(RouteGenerator.home);
+                        } else {
+                          debugPrint("an error occured when signing in!");
+                        }
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("PROCEED"),
+                          Icon(Icons.arrow_right_rounded),
+                        ],
+                      ),
                     ),
-                  ),
-                )
-              ],
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: Container(
+                        alignment: Alignment.bottomCenter,
+                        // color: Colors.white,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pushNamed(
+                                    context, RouteGenerator.forgotPassword);
+                              },
+                              child: const Text("Forgot Password?"),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context)
+                                    .pushNamed(RouteGenerator.signUp);
+                              },
+                              child: const Text("Create Account"),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
         ),
--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,9 +1,7 @@
-import 'package:assignment/screens/sign_in.dart';
-import 'package:assignment/controller/authentication.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-import 'create_profile.dart';
+import 'package:assignment/route.dart';
+import 'package:provider/provider.dart';
+import '../controller/authentication.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -14,133 +12,133 @@ class SignUp extends StatefulWidget {
 
 class _SignUpState extends State<SignUp> {
   final GlobalKey<FormState> formkey = GlobalKey<FormState>();
-  final Auth auth = Auth();
   TextEditingController usernameText = TextEditingController();
   TextEditingController passwordText = TextEditingController();
-  bool? lightTheme;
   @override
   Widget build(BuildContext context) {
-    lightTheme = Theme.of(context).brightness == Brightness.dark;
-    return Container(
-      decoration: lightTheme!
-          ? const BoxDecoration(
-              gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color.fromARGB(255, 40, 80, 110),
-                Color.fromARGB(255, 23, 47, 73),
-              ],
-            ))
-          : const BoxDecoration(),
+    final auth = Provider.of<Auth>(context);
+    bool lightTheme = Theme.of(context).brightness == Brightness.dark;
+    return SafeArea(
       child: Scaffold(
-        backgroundColor: !lightTheme! ? Colors.white : Colors.transparent,
-        body: Center(
+        backgroundColor: lightTheme ? Colors.transparent : Colors.white,
+        body: SingleChildScrollView(
           child: Container(
-            constraints: const BoxConstraints(maxWidth: 700),
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ClipOval(
-                  child: Container(
-                    height: 40,
-                    width: 40,
-                    color: Colors.grey,
-                  ),
-                ),
-                const SizedBox(height: 100),
-                Text(
-                  "Create Your Account",
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                Text(
-                  "Sign up to continue",
-                  style: Theme.of(context).textTheme.subtitle2,
-                ),
-                const SizedBox(height: 60),
-                TextFormField(
-                  controller: usernameText,
-                  decoration: const InputDecoration(labelText: "USERNAME"),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: passwordText,
-                  obscureText: true,
-                  decoration: const InputDecoration(labelText: "PASSWORD"),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () async {
-                    var email = usernameText.text;
-                    var password = passwordText.text;
-
-                    User? user = await auth
-                        .signUpWithEmailAndPassword(
-                            email: email, password: password)
-                        .onError(
-                      (error, stackTrace) async {
-                        ScaffoldMessenger.of(context).showMaterialBanner(
-                          MaterialBanner(
-                            content: Text("$error"),
-                            actions: [
-                              TextButton(
-                                onPressed: () async {
-                                  ScaffoldMessenger.of(context)
-                                      .clearMaterialBanners();
-                                },
-                                child: const Text("Dismiss"),
-                              )
-                            ],
-                          ),
-                        );
-                        await Future.delayed(const Duration(seconds: 3));
-                        ScaffoldMessenger.of(context).clearMaterialBanners();
-                      },
-                    );
-                    if (user != null) {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (BuildContext context) =>
-                                  CreateProfile()));
-                    }
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text("PROCEED"),
-                      Icon(Icons.arrow_right_rounded),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Expanded(
-                  child: Container(
-                    alignment: Alignment.bottomCenter,
-                    // color: Colors.white,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    const SignIn(),
-                              ),
-                            );
-                          },
-                          child: const Text("Sign In"),
-                        ),
-                        const Text(""),
+            height: MediaQuery.of(context).size.height - 30,
+            decoration: lightTheme
+                ? const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color.fromARGB(255, 40, 80, 110),
+                        Color.fromARGB(255, 23, 47, 73),
                       ],
                     ),
-                  ),
-                )
-              ],
+                  )
+                : const BoxDecoration(),
+            child: Center(
+              child: Container(
+                constraints: const BoxConstraints(maxWidth: 700),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipOval(
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(height: 100),
+                    Text(
+                      "Create Your Account",
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    Text(
+                      "Sign up to continue",
+                      style: Theme.of(context).textTheme.subtitle2,
+                    ),
+                    const SizedBox(height: 60),
+                    TextFormField(
+                      controller: usernameText,
+                      decoration: const InputDecoration(labelText: "USERNAME"),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: passwordText,
+                      obscureText: true,
+                      decoration: const InputDecoration(labelText: "PASSWORD"),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () async {
+                        var email = usernameText.text;
+                        var password = passwordText.text;
+
+                        await auth
+                            .signUpWithEmailAndPassword(
+                                email: email, password: password)
+                            .onError(
+                          (error, stackTrace) async {
+                            ScaffoldMessenger.of(context).showMaterialBanner(
+                              MaterialBanner(
+                                content: Text("$error"),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      ScaffoldMessenger.of(context)
+                                          .clearMaterialBanners();
+                                    },
+                                    child: const Text("Dismiss"),
+                                  )
+                                ],
+                              ),
+                            );
+                            await Future.delayed(const Duration(seconds: 3));
+                            ScaffoldMessenger.of(context)
+                                .clearMaterialBanners();
+                            return null;
+                          },
+                        );
+                        if (auth.checkUser()) {
+                          Navigator.pushNamed(
+                              context, RouteGenerator.createProfile);
+                        }
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("PROCEED"),
+                          Icon(Icons.arrow_right_rounded),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: Container(
+                        alignment: Alignment.bottomCenter,
+                        // color: Colors.white,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pushNamed(
+                                    context, RouteGenerator.signIn);
+                              },
+                              child: const Text("Sign In"),
+                            ),
+                            const Text(""),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
         ),
