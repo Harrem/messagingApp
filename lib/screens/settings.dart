a20dc63129f281fcddab143d1f2e5e843a4af756
Harrem <52822616+Harrem@users.noreply.github.com>
2022-10-17 21:42:37 +0300

    designed user profile picture

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -10,25 +10,62 @@ class Settings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final userProvider = Provider.of<UserActions>(context);
+    final userActions = Provider.of<UserActions>(context);
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.info_outline)),
+            ),
+          ],
+        ),
         body: SizedBox(
           width: double.infinity,
           child: Column(
             children: [
-              const OvalPicture(size: 70),
-              TextButton(
+              const SizedBox(height: 30),
+              Stack(
+                alignment: Alignment.bottomRight,
+                children: [
+                  const Icon(Icons.edit),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(
+                          context, RouteGenerator.editProfilePic);
+                    },
+                    child: OvalPicture(
+                        image: userActions.userData.profilePictureUrl.isNotEmpty
+                            ? Image.network(
+                                userActions.userData.profilePictureUrl)
+                            : null,
+                        size: 130),
+                  ),
+                ],
+              ),
+              const Divider(),
+              const Divider(),
+              Text(
+                "${userActions.userData.firstName} ${userActions.userData.lastName}",
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              const Divider(),
+              const ListTile(
+                title: Text("System Theme"),
+                leading: Icon(Icons.brightness_6),
+              ),
+              const Divider(),
+              OutlinedButton(
                 onPressed: () {
-                  Auth().signout();
-                  if (Auth().firebaseAuth.currentUser == null) {
-                    Navigator.pushNamed(context, RouteGenerator.signIn);
-                  }
+                  Auth().signout().then((value) =>
+                      Navigator.pushNamed(context, RouteGenerator.signIn));
+                  // if (Auth().firebaseAuth.currentUser == null) {
+                  // }
                 },
                 child: const Text("Sign out"),
               ),
-              Text(
-                  "${userProvider.userData.firstName} ${userProvider.userData.lastName}")
             ],
           ),
         ),
