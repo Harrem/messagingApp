b71ee36b7c8fd31990b5730eaa2cd204ecb4a226
Harrem <52822616+Harrem@users.noreply.github.com>
2022-10-17 21:45:33 +0300

    added future builder for getting User Data

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,42 +14,11 @@ class Home extends StatelessWidget {
     final userActions = Provider.of<UserActions>(context);
     final navController = Provider.of<BottomNavController>(context);
     return WillPopScope(
-      onWillPop: () async {
-        return false;
-      },
-      child: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              "Chats",
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            leading: Container(
-              padding: const EdgeInsets.all(5),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, RouteGenerator.settingPage);
-                },
-                child: OvalPicture(
-                    image: userActions.userData.profilePictureUrl.isNotEmpty
-                        ? Image.network(userActions.userData.profilePictureUrl)
-                        : null,
-                    size: 50),
-              ),
-            ),
-            actions: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                child: IconButton(
-                    onPressed: () {
-                      Provider.of<ThemeController>(context, listen: false)
-                          .turnThemeMode();
-                    },
-                    icon: const Icon(Icons.light_mode)),
-              )
-            ],
-          ),
-          body: FutureBuilder(
+        onWillPop: () async {
+          return false;
+        },
+        child: SafeArea(
+          child: FutureBuilder(
             future: userActions.getUserData(),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
@@ -57,89 +26,123 @@ class Home extends StatelessWidget {
               } else if (snapshot.hasError) {
                 return const Center(child: Text("Error Happened"));
               }
-              return Column(
-                children: [
-                  Expanded(
-                    child: FutureBuilder<List<Conversation>?>(
-                        future: userActions.getConversations(),
-                        builder: (context, snapshot) {
-                          final covs = snapshot.data;
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text(
+                    "Chats",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  leading: Container(
+                    padding: const EdgeInsets.all(5),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(
+                            context, RouteGenerator.settingPage);
+                      },
+                      child: OvalPicture(
+                          image:
+                              userActions.userData.profilePictureUrl.isNotEmpty
+                                  ? Image.network(
+                                      userActions.userData.profilePictureUrl)
+                                  : null,
+                          size: 50),
+                    ),
+                  ),
+                  actions: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: IconButton(
+                          onPressed: () {
+                            Provider.of<ThemeController>(context, listen: false)
+                                .turnThemeMode();
+                          },
+                          icon: const Icon(Icons.light_mode)),
+                    )
+                  ],
+                ),
+                body: Column(
+                  children: [
+                    Expanded(
+                      child: FutureBuilder<List<Conversation>?>(
+                          future: userActions.getConversations(),
+                          builder: (context, snapshot) {
+                            final covs = snapshot.data;
 
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const Center(
-                                child: CircularProgressIndicator());
-                          } else if (snapshot.connectionState ==
-                              ConnectionState.done) {
-                            if (snapshot.hasData) {
-                              return ListView.builder(
-                                itemCount: snapshot.data!.length,
-                                shrinkWrap: true,
-                                itemBuilder: ((context, index) {
-                                  return Column(
-                                    children: [
-                                      ConvTile(
-                                        title: snapshot
-                                            .data![index].conversationId,
-                                        lastMessage:
-                                            covs![index].messages.isNotEmpty
-                                                ? covs[index].messages.last.text
-                                                : "no messages",
-                                      ),
-                                      const SizedBox(height: 15),
-                                    ],
-                                  );
-                                }),
-                              );
-                            } else if (snapshot.hasError) {
-                              return Center(
-                                child: Text(snapshot.error.toString()),
-                              );
-                            } else {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
                               return const Center(
-                                child: Text("Data is null"),
-                              );
+                                  child: CircularProgressIndicator());
+                            } else if (snapshot.connectionState ==
+                                ConnectionState.done) {
+                              if (snapshot.hasData) {
+                                return ListView.builder(
+                                  itemCount: snapshot.data!.length,
+                                  shrinkWrap: true,
+                                  itemBuilder: ((context, index) {
+                                    return Column(
+                                      children: [
+                                        ConvTile(
+                                          title: snapshot
+                                              .data![index].conversationId,
+                                          lastMessage: covs![index]
+                                                  .messages
+                                                  .isNotEmpty
+                                              ? covs[index].messages.last.text
+                                              : "no messages",
+                                        ),
+                                        const SizedBox(height: 15),
+                                      ],
+                                    );
+                                  }),
+                                );
+                              } else if (snapshot.hasError) {
+                                return Center(
+                                  child: Text(snapshot.error.toString()),
+                                );
+                              } else {
+                                return const Center(
+                                  child: Text("Data is null"),
+                                );
+                              }
                             }
-                          }
-                          return const Center(
-                            child: Text("Connnection Error"),
-                          );
-                        }),
-                  )
-                ],
+                            return const Center(
+                              child: Text("Connnection Error"),
+                            );
+                          }),
+                    )
+                  ],
+                ),
+                bottomNavigationBar: BottomAppBar(
+                  child: BottomNavigationBar(
+                    currentIndex: navController.index,
+                    onTap: (value) {
+                      navController.setIndex(value);
+                    },
+                    items: const [
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.home),
+                        label: "Home",
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.phone),
+                        label: "Calls",
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.person),
+                        label: "Actives",
+                      ),
+                    ],
+                  ),
+                ),
+                floatingActionButton: FloatingActionButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, RouteGenerator.searchPage);
+                  },
+                  child: const Icon(Icons.search),
+                ),
               );
             },
           ),
-          bottomNavigationBar: BottomAppBar(
-            child: BottomNavigationBar(
-              currentIndex: navController.index,
-              onTap: (value) {
-                navController.setIndex(value);
-              },
-              items: const [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: "Home",
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.phone),
-                  label: "Calls",
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.person),
-                  label: "Actives",
-                ),
-              ],
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              Navigator.pushNamed(context, RouteGenerator.searchPage);
-            },
-            child: const Icon(Icons.search),
-          ),
-        ),
-      ),
-    );
+        ));
   }
 }
