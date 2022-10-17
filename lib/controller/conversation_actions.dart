4daad8c87dd673ec7de02e3afd4382e50c2f03d4
Harrem <52822616+Harrem@users.noreply.github.com>
2022-10-17 21:42:53 +0300

    added messaging methods

--- lib/controller/conversation_actions.dart
+++ lib/controller/conversation_actions.dart
@@ -1,15 +1,40 @@
+import 'package:assignment/models/user_data.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import '../models/conversations.dart';
 
 class ConversationActions extends ChangeNotifier {
-  Future<List<Conversation>> getConversations() async {
-    var docRef = FirebaseFirestore.instance.collection("users").doc().get();
-    var doc = await docRef;
-    var data = doc.data();
-    var conversations = data!['conversations']
-        .map((e) => Conversation.fromMap(e.data()))
-        .toList();
-    return conversations;
+  final firestore = FirebaseFirestore.instance;
+  final firebaseAuth = FirebaseAuth.instance;
+  String uid = "";
+
+  void setMessageUserid(String uid) {
+    this.uid = uid;
   }
+
+  String getMessageUid() {
+    return uid;
+  }
+
+  Future<void> createConversation(String toUid, UserData userData) async {
+    final docRef =
+        await firestore.collection("conversations").add({'toUid': toUid});
+    final docId = docRef.id;
+    DocumentReference ref = docRef;
+    firestore
+        .collection('users')
+        .doc(firebaseAuth.currentUser!.uid)
+        .update({'conversations': docRef});
+  }
+
+  // Future<List<Conversation>> getConversations() async {
+  //   var docRef = FirebaseFirestore.instance.collection("users").doc(uid).get();
+  //   var doc = await docRef;
+  //   var data = doc.data();
+  //   if (data == null) return Future.error('Start a conversation');
+  //   List<Conversations> conversations = data['conversations']
+  //       .map((e) => Conversation.fromMap(e.data()))
+  //       .toList();
+  //   return conversations;
+  // }
 }
