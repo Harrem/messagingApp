3e6bb26cf35b91653dd281813774861c51e56fec
Harrem <52822616+Harrem@users.noreply.github.com>
2022-10-17 21:46:17 +0300

    Minimal changes

--- lib/screens/messaging.dart
+++ lib/screens/messaging.dart
@@ -1,6 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:assignment/controller/conversation_actions.dart';
+import 'package:assignment/controller/user_profile_actions.dart';
 import 'package:flutter/material.dart';
 import 'package:assignment/services/cloudStore.dart';
+import 'package:provider/provider.dart';
 
 class MessageScreen extends StatefulWidget {
   const MessageScreen({Key? key}) : super(key: key);
@@ -16,6 +18,7 @@ class _MessageScreenState extends State<MessageScreen> {
   String username = "";
   @override
   Widget build(BuildContext context) {
+    final userActions = Provider.of<UserActions>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Harrem"),
@@ -28,36 +31,38 @@ class _MessageScreenState extends State<MessageScreen> {
             Expanded(
               child: SizedBox(
                 width: double.infinity,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: 100,
-                  //  snapshot.data.docs.length,
-                  itemBuilder: ((context, index) {
-                    return Container(
-                      margin: const EdgeInsets.all(10),
-                      padding: const EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: Colors.grey[900]),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            "Harrem Mhamad",
-                            // snapshot.data.docs[index]['user'],
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            "yo, sup?",
-                            // snapshot.data.docs[index]['msg'],
-                            style: TextStyle(color: Colors.grey[700]),
-                          ),
-                        ],
-                      ),
-                    );
-                  }),
+                child: FutureBuilder(
+                  builder: (context, snapshot) => ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: 10,
+                    //  snapshot.data.docs.length,
+                    itemBuilder: ((context, index) {
+                      return Container(
+                        margin: const EdgeInsets.all(10),
+                        padding: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.grey[900]),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Harrem Mhamad",
+                              // snapshot.data.docs[index]['user'],
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              "yo, sup?",
+                              // snapshot.data.docs[index]['msg'],
+                              style: TextStyle(color: Colors.grey[700]),
+                            ),
+                          ],
+                        ),
+                      );
+                    }),
+                  ),
                 ),
               ),
             ),
