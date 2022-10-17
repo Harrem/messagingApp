7e91f9f7640b07ddfbd7b71e2368ea831d4bd3e0
Harrem <52822616+Harrem@users.noreply.github.com>
2022-10-17 21:43:49 +0300

    minimal changes

--- lib/models/conversations.dart
+++ lib/models/conversations.dart
@@ -1,54 +1,57 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 
 import 'message.dart';
 
 class Conversation {
-  String? conversationId;
-  String? createdDate;
-  List<Message>? messages;
+  String conversationId;
+  DateTime createdDate;
+  List<Message> messages;
+  String docRef;
 
   Conversation({
-    this.conversationId,
-    this.createdDate,
-    this.messages,
+    required this.conversationId,
+    required this.createdDate,
+    required this.messages,
+    required this.docRef,
   });
 
   Conversation copyWith({
     String? conversationId,
-    String? createdDate,
+    DateTime? createdDate,
     List<Message>? messages,
+    String? docRef,
   }) {
     return Conversation(
       conversationId: conversationId ?? this.conversationId,
       createdDate: createdDate ?? this.createdDate,
       messages: messages ?? this.messages,
+      docRef: docRef ?? this.docRef,
     );
   }
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'conversationId': conversationId,
-      'createdDate': createdDate,
-      'messages': messages!.map((x) => x.toMap()).toList(),
+      'createdDate': createdDate.millisecondsSinceEpoch,
+      'messages': messages.map((x) => x.toMap()).toList(),
+      'docRef': docRef,
     };
   }
 
   factory Conversation.fromMap(Map<String, dynamic> map) {
     return Conversation(
-      conversationId: map['conversationId'] != null
-          ? map['conversationId'] as String
-          : null,
+      conversationId: map['conversationId'] as String,
       createdDate:
-          map['createdDate'] != null ? map['createdDate'] as String : null,
-      messages: map['messages'] != null
-          ? List<Message>.from(
-              (map['messages'] as List<int>).map<Message?>(
-                (x) => Message.fromMap(x as Map<String, dynamic>),
-              ),
-            )
-          : null,
+          DateTime.fromMillisecondsSinceEpoch(map['createdDate'] as int),
+      messages: List<Message>.from(
+        (map['messages'] as List<dynamic>).map<Message>(
+          (x) => Message.fromMap(x as Map<String, dynamic>),
+        ),
+      ),
+      docRef: map['docRef'] as String,
     );
   }
 
@@ -58,8 +61,9 @@ class Conversation {
       Conversation.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() =>
-      'Conversation(conversationId: $conversationId, createdDate: $createdDate, messages: $messages)';
+  String toString() {
+    return 'Conversation(conversationId: $conversationId, createdDate: $createdDate, messages: $messages, docRef: $docRef)';
+  }
 
   @override
   bool operator ==(covariant Conversation other) {
@@ -67,10 +71,15 @@ class Conversation {
 
     return other.conversationId == conversationId &&
         other.createdDate == createdDate &&
-        listEquals(other.messages, messages);
+        listEquals(other.messages, messages) &&
+        other.docRef == docRef;
   }
 
   @override
-  int get hashCode =>
-      conversationId.hashCode ^ createdDate.hashCode ^ messages.hashCode;
+  int get hashCode {
+    return conversationId.hashCode ^
+        createdDate.hashCode ^
+        messages.hashCode ^
+        docRef.hashCode;
+  }
 }
